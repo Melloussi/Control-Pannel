6c5627582a123facd0217dc304492bb30d81eb56
TesterCoder06 <soskodev@gmail.com>
2022-10-17 00:41:12 +0100

    update

--- server.js
+++ server.js
@@ -86,4 +86,6 @@ function checkNotAuthenticated(req, res, next) {
   next()
 }
 
-app.listen(3000)
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app
